813f8a91cd18ac9cedf77f1b5a358ab935c9a3c4
pancham99 <pancham.047@gmail.com>
2023-03-24 00:15:44 +0530

    add new component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react' 
-// import About from './pages/About'
-// import Hero from './pages/Hero'
-// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import About from './pages/About'
-import Header from './pages/Header'
-import Hero from './pages/Hero'
-
-// const router =createBrowserRouter([
-//   {
-//     path:'/'
-//     element: <Hero />
-//   }
-// ])
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Home from './pages/Home'
 
 const App = () => {
   return (
     <>
-    <Header />
-    <Hero />
-    <About />
+   <Header/>
+<Hero/>
+   <Home/>
+    
+    
   
     </>
   )
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,9 @@ import React from 'react'
 const Header = () => {
     return (
         <>
-            <header className='bg-white h-20 flex items-center justify-between px-8'>
+            <header className='bg-white h-26 p-2 flex items-center justify-between px-8 sticky top-0 z-20'>
                 <div>
-                    <img className='h-[10vh] w-[30vh] outline-none' src='https://static.startuptalky.com/2021/07/livspace-logo-startuptalky.jpg' alt='' />
+                    <img className='w-20 outline-none ' src='https://www.livspace.com/_nuxt/img/companyLogoWithBrand.7384cb2.png' alt='' />
                 </div>
                 <div className= ''>
                     <button type='Submit' className='btn btn-primary bg-red-500 text-white text-sm py-2 px-8 rounded-full uppercase' >get free quote</button>
@@ -17,4 +17,4 @@ const Header = () => {
     )
 }
 
-export default Header
+export default Header;
