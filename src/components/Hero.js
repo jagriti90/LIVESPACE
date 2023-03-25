a569995cc65830c94d53e63e8afc7e9ca6394aef
pancham99 <pancham.047@gmail.com>
2023-03-25 23:04:12 +0530

    add validation

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,6 +4,7 @@ import React, { useState } from 'react'
 // import img1 from '..///'
 
 const Hero = () => {
+  const [errors, setErrors] = useState({})
   const [user, setUser] = useState({
     name: "",
     email: "",
@@ -53,36 +54,28 @@ const Hero = () => {
 
       }
 
-    } else {
-       alert("please fill all data")
+    } else{
+      setErrors(validation(user))
     }
+     
+  }
 
-    const res = await fetch("https://livespace-fb11d-default-rtdb.firebaseio.com/livespacehouseform.json",
-      {
-        method: "POST",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify({
-          name,
-          email,
-          phone,
-          property
-
-        })
-      });
-
-    if (res) {
-      setUser({
-        name: "",
-        email: "",
-        phone: "",
-        property: ""
-      });
-      alert("form submit successfully")
-
+  const validation = (user) => {
+    let errors = {};
+    if (!user.name) {
+      errors.name = "fill the name please"
+    } if (!user.email) {
+      errors.email = "please fill the email"
+    } else if (!/\S+@\S+\.\S+/.test(user.email)) {
+      errors.email = "email is invilid"
+    } if (!user.phone) {
+      errors.phone = "please fill the phone number"
+    } else if (user.phone.length <= 10) {
+      errors.phone = "plese fill the 10 number"
+    } if (!user.property) {
+      errors.property = "please fill the property"
     }
-
+    return errors;
   }
 
   console.log(user, "jjj")
@@ -113,12 +106,15 @@ const Hero = () => {
                   <h1 className='text-2xl font-bold'>Designs for Every Budget</h1>
                   <div className='mb-3 pt-8'>
                     <input className='px-3 py-4 w-[50vh] outline-2 outline-amber-900 placeholder-slate-300 text-slate-600 relative bg-white  rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring ' type="text" value={user.name} onChange={getUserData} name="name" placeholder='Name' />
+                    {errors.name && <p className='text-red-500 text-xs text-left ml-16 mt-[1px]'>{errors.name}</p>}
                   </div>
                   <div className='mb-3 pt-4'>
                     <input className='px-3 py-4 w-[50vh] outline-2 outline-amber-900 placeholder-slate-300 text-slate-600 relative bg-white  rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring ' type="text" value={user.email} onChange={getUserData} name="email" placeholder='Email id' />
+                    {errors.email && <p className='text-red-500 text-xs text-left ml-16 mt-[1px]'>{errors.email}</p>}
                   </div>
                   <div className='mb-3 pt-4'>
                     <input className='px-3 py-4 w-[50vh] outline-2 outline-amber-900 placeholder-slate-300 text-slate-600 relative bg-white  rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring ' type='text' value={user.phone} onChange={getUserData} name='phone' placeholder='Phone number' />
+                    {errors.phone && <p className='text-red-500 text-xs text-left ml-16 mt-[1px]'>{errors.phone}</p>}
                   </div>
                   <div>
                     <input type="checkbox" name='whatsapp' />
@@ -127,6 +123,7 @@ const Hero = () => {
                   {/* <input type='Email id' placeholder='Email id' /> */}
                   <div className='mb-3 pt-4'>
                     <input className='px-3 py-4 w-[50vh] outline-2 outline-amber-900 placeholder-slate-300 text-slate-600 relative bg-white  rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring ' type='text' value={user.property} onChange={getUserData} name='property' placeholder='Property Name' />
+                    {errors.property && <p className='text-red-500 text-xs text-left ml-16 mt-[1px]'>{errors.property}</p>}
                   </div>
                   <div class="mb-3 pt-3">
                     <button onClick={postData} type='Submit' className='btn btn-primary bg-red-600 text-white py-4 px-8 rounded-full uppercase' >Submit</button>
@@ -146,4 +143,4 @@ const Hero = () => {
   )
 }
 
-export default Hero
+export default Hero;
